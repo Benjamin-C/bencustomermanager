b8ffe1b119baef5806941011d6a27cd5c0a4792d
Benjamin-C <bencrall@gmail.com>
2020-11-21 11:27:49 -0800

    fixed quotes becoming unescaped when becoming html

--- new.js
+++ new.js
@@ -2,54 +2,54 @@ let new_type = "";
 let new_uuid = "";
 
 function setupNewPerson(type, div, uuid) {
-  // new_type = type;
-  // new_uuid = uuid;
-  // document.getElementById(div).innerHTML = "<div id=\"titlediv\"></div><div id=\"formdiv\"></div>";
-  // new_onSelect();
+  new_type = type;
+  new_uuid = uuid;
+  document.getElementById(div).innerHTML = "<div id=\"titlediv\"></div><div id=\"formdiv\"></div>";
+  new_onSelect();
 }
 
 function new_onSelect() {
-  // sendRequest('action=fields&table=' + new_type, new_setupPage);
+  sendRequest('action=fields&table=' + new_type, new_setupPage);
 }
 
 function new_setupPage(msg) {
-  // let h = "";
-  // h += "<form id=\"newform\" action=\"db\" method=\"POST\">";
-  // switch(new_type) {
-  // case "people": {
-  //   document.getElementById("titlediv").innerHTML = "<b>New Person</b>";
-  // } break;
-  // case "orders": {
-  //   document.getElementById("titlediv").innerHTML = "<b>New Order</b>";
-  //   h += "<input type=\"hidden\" name=\"puuid\" value=\"" + new_uuid + "\">";
-  // } break;
-  // }
-  // h += "<input type=\"hidden\" name=\"action\" value=\"new\">";
-  // h += "<input type=\"hidden\" name=\"table\" value=\"" + new_type + "\">";
-  // var parser = new DOMParser();
-  // var doc = parser.parseFromString(msg, 'text/html');
-  // var table = doc.getElementById('data');
-  // let num = table.rows.length;
-  // let firstField = true;
-  // for(let i = 1; i < num; i++) {
-  //   let name = table.rows[i].cells[0].textContent;
-  //   if(name != "uuid_bin" && name != "uuid" && name != "puuid" && name != "puuid_bin" && name != "id") {
-  //     if(firstField) {
-  //       firstField = false;
-  //     } else {
-  //       fields += ",";
-  //     }
-  //     fields += name;
-  //     // console.log(":" + name + ":");
-  //     h += "<label for=\"" + name + "\">" + name + "</label>"
-  //     h += "<input type=\"text\" name=\"" + name + "\"><br/>";
-  //   }
-  // }
-  // h += "<input type=\"submit\" value=\"Submit\">";
-  // h += "<button onclick=\"edit()\" id=\"edit_button\">Edit</button><br/>";
-  // h += "</form>";
-  // h += "";
-  // document.getElementById("formdiv").innerHTML = h;
-  // document.getElementById("newform").addEventListener("submit", asyncSubmitForm);
-  // // document.getElementById("authform").addEventListener("submit", asyncSubmitForm(e, (data) => {alert(data)});});
+  let h = "";
+  h += "<form id=\"newform\" action=\"db\" method=\"POST\">";
+  switch(new_type) {
+  case "people": {
+    document.getElementById("titlediv").innerHTML = "<b>New Person</b>";
+  } break;
+  case "orders": {
+    document.getElementById("titlediv").innerHTML = "<b>New Order</b>";
+    h += "<input type=\"hidden\" name=\"puuid\" value=\"" + new_uuid + "\">";
+  } break;
+  }
+  h += "<input type=\"hidden\" name=\"action\" value=\"new\">";
+  h += "<input type=\"hidden\" name=\"table\" value=\"" + new_type + "\">";
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(msg, 'text/html');
+  var table = doc.getElementById('data');
+  let num = table.rows.length;
+  let firstField = true;
+  for(let i = 1; i < num; i++) {
+    let name = table.rows[i].cells[0].textContent;
+    if(name != "uuid_bin" && name != "uuid" && name != "puuid" && name != "puuid_bin" && name != "id") {
+      if(firstField) {
+        firstField = false;
+      } else {
+        fields += ",";
+      }
+      fields += name;
+      // console.log(":" + name + ":");
+      h += "<label for=\"" + name + "\">" + name + "</label>"
+      h += "<input type=\"text\" name=\"" + name + "\"><br/>";
+    }
+  }
+  h += "<input type=\"submit\" value=\"Submit\">";
+  h += "<button onclick=\"edit()\" id=\"edit_button\">Edit</button><br/>";
+  h += "</form>";
+  h += "";
+  document.getElementById("formdiv").innerHTML = h;
+  document.getElementById("newform").addEventListener("submit", asyncSubmitForm);
+  // document.getElementById("authform").addEventListener("submit", asyncSubmitForm(e, (data) => {alert(data)});});
 }
